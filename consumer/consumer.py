59cd684cc94e42cfbb15c6dc7eae6b7487bcc568
Nicolas Anelli <nickanelli@hotmail.com>
2020-11-25 00:30:28 -0300

    estrutura mais simples

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,22 +1,15 @@
 #!/usr/bin/env python
 import pika
-import time
-from random import randrange
+
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
 channel = connection.channel()
 
 channel.queue_declare(queue='first')
-channel.queue_declare(queue='second')
-channel.queue_declare(queue='third')
 
 def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
-    time.sleep(randrange(0, 5))
-    print(" [x] Done")
-    ch.basic_ack(delivery_tag = method.delivery_tag)
+    print(" [x] Received [%r]" % body)
 
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume('first', callback)
+channel.basic_consume('first', callback, auto_ack=True)
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
